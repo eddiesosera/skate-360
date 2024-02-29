2a46f75ece18aef4c72d0b198473c34ba07f8989
Ungerer221 <221302@virtualwindow.co.za>
2024-02-29 12:50:56 +0200

    side navbar under construction

--- client/src/app/components/navigation/sidebar/sidebar.component.ts
+++ client/src/app/components/navigation/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TooltipComponent } from '../../feedback/tooltip/tooltip.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [TooltipComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
