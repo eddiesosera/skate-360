df45598f30b227d372c59af12be8184eb954ae9d
Ungerer221 <221302@virtualwindow.co.za>
2024-02-29 08:38:13 +0200

    warehouse page

--- client/src/app/pages/wharehouse/wharehouse.component.ts
+++ client/src/app/pages/wharehouse/wharehouse.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SearchBarComponent } from '../../components/inputs/search-bar/search-bar.component';
+import { SidebarComponent } from '../../components/navigation/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-wharehouse',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, SearchBarComponent, SidebarComponent],
   templateUrl: './wharehouse.component.html',
   styleUrl: './wharehouse.component.css'
 })
