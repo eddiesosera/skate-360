b96d011e9ea3cc810ea9163bd730e6860c6a1219
Ungerer221 <221302@virtualwindow.co.za>
2024-02-29 18:26:54 +0200

    ware house and side bar

--- client/src/app/components/navigation/sidebar/sidebar.component.ts
+++ client/src/app/components/navigation/sidebar/sidebar.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { TooltipComponent } from '../../feedback/tooltip/tooltip.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavItem } from '../../../models/components/navigation/navbar.models';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [TooltipComponent],
+  imports: [CommonModule, TooltipComponent, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent{
 
+  navItems: NavItem[] =[
+    {
+      link: 'craft',
+      icon: 'ri-tools-fill',
+      title:'Craft'
+    },
+    {
+      link: 'skateboards',
+      icon:'ri-box-2-line',
+      title:'Skateboards'
+    },
+    {
+      link: 'wharehouse',
+      icon:'ri-building-2-line',
+      title:'Wharehouse'
+    },
+    {
+      link: 'account',
+      icon: 'ri-user-3-line',
+      title: 'Account',
+    },
+  ];
 }
