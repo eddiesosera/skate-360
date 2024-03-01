cc0e53cdbb9e6568477818ba888a55a68a848086
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-01 12:44:38 +0200

    Home: Created Sections and added Image Assets

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonComponent } from '../../components/inputs/button/button.component';
+import { NewsletterComponent } from './sections/newsletter/newsletter.component';
+import { OurCollectionComponent } from './sections/our-collection/our-collection.component';
+import { LocationsComponent } from './sections/locations/locations.component';
+import { HeroComponent } from './sections/hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, ButtonComponent],
+  imports: [
+    RouterLink,
+    ButtonComponent,
+    HeroComponent,
+    NewsletterComponent,
+    OurCollectionComponent,
+    LocationsComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ client/src/app/pages/home/sections/our-collection/our-collection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-collection',
+  standalone: true,
+  imports: [],
+  templateUrl: './our-collection.component.html',
+  styleUrl: './our-collection.component.css'
+})
+export class OurCollectionComponent {
+
+}
