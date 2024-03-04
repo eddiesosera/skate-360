cb88f211a25aec9c59dd4f5daf719aafa33855e0
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-04 23:46:39 +0200

    Home (Newsletter): Grid system Init
    Installed:
    - Gridstack for draggable elements
    - Masonry Layout: for grid layout cards

---
+++ client/src/app/pages/home/sections/our-collection/dummy data.ts
@@ -0,0 +1,67 @@
+export const dummdata: object[] = [
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+    {
+        id: 1,
+        type: 'legacy'
+    },
+
+]
--- client/src/app/pages/home/sections/our-collection/our-collection.component.ts
+++ client/src/app/pages/home/sections/our-collection/our-collection.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { ButtonComponent } from '../../../../components/inputs/button/button.component';
+import { dummdata } from './dummy data';
+import Masonry from 'masonry-layout';
+import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from '../../../../components/data-display/product-card/product-card.component';
 
 @Component({
   selector: 'app-our-collection',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [CommonModule, ProductCardComponent, ButtonComponent],
   templateUrl: './our-collection.component.html',
   styleUrl: './our-collection.component.css'
 })
-export class OurCollectionComponent {
+export class OurCollectionComponent implements AfterViewInit {
+  skateboardCollection = dummdata
+  masonry = new Masonry
+
+  ngAfterViewInit(): void {
+    var grid = document.querySelector('.grid');
+    var masonry = new Masonry(grid!, {
+      itemSelector: '.grid-item',
+      // columnWidth: 200,
+      percentPosition: true,
+      gutter: 10
+    });
+  }
 
 }
