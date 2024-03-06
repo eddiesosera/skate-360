cc90f83a17c114c28c5196c4a25349de593c4da2
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-07 00:29:18 +0200

    Home (Locations) Layout Style

--- client/src/app/pages/home/sections/locations/locations.component.ts
+++ client/src/app/pages/home/sections/locations/locations.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { GridlayoutComponent } from '../../../../components/data-display/gridlayout/gridlayout.component';
 
 @Component({
   selector: 'app-locations',
   standalone: true,
-  imports: [],
+  imports: [GridlayoutComponent],
   templateUrl: './locations.component.html',
   styleUrl: './locations.component.css'
 })
