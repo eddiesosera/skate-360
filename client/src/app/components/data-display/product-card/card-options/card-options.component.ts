88f2c35d0d042a7b66f60c19899ecde0f9c90aee
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-07 00:11:18 +0200

    Card Options Styling v1

--- client/src/app/components/data-display/product-card/card-options/card-options.component.ts
+++ client/src/app/components/data-display/product-card/card-options/card-options.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component } from '@angular/core';
+import { ButtonComponent } from '../../../inputs/button/button.component';
 
 @Component({
   selector: 'app-card-options',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './card-options.component.html',
   styleUrl: './card-options.component.css'
 })
-export class CardOptionsComponent {
+export class CardOptionsComponent implements AfterViewInit {
+  isOptionVisible: boolean = false
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit(): void {
+
+  }
+
+  viewOptions() {
+    this.isOptionVisible = !this.isOptionVisible
+  }
 
 }
