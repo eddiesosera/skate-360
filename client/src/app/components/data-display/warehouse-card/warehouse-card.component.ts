2be19d0cb7f2aeaee883fb93ca3e518ea91040e0
Ungerer221 <221302@virtualwindow.co.za>
2024-03-11 11:05:41 +0200

    added the warehouse card component
    not done yet just need to import data and the hero image and more

---
+++ client/src/app/components/data-display/warehouse-card/warehouse-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warehouse-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './warehouse-card.component.html',
+  styleUrl: './warehouse-card.component.css'
+})
+export class WarehouseCardComponent {
+
+}
--- client/src/app/pages/wharehouse/wharehouse.component.ts
+++ client/src/app/pages/wharehouse/wharehouse.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SearchBarComponent } from '../../components/inputs/search-bar/search-bar.component';
 import { SidebarComponent } from '../../components/navigation/sidebar/sidebar.component';
+import { WarehouseCardComponent } from '../../components/data-display/warehouse-card/warehouse-card.component';
 
 @Component({
   selector: 'app-wharehouse',
   standalone: true,
-  imports: [RouterLink, SearchBarComponent, SidebarComponent],
+  imports: [RouterLink, SearchBarComponent, SidebarComponent,WarehouseCardComponent],
   templateUrl: './wharehouse.component.html',
   styleUrl: './wharehouse.component.css'
 })
