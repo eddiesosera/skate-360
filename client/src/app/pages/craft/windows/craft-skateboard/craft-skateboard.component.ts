8dce0e38b94a91c019739daf85087b48fff92453
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-12 00:40:42 +0200

    Craft Skateboard Structure

--- client/src/app/pages/craft/windows/craft-skateboard/craft-skateboard.component.ts
+++ client/src/app/pages/craft/windows/craft-skateboard/craft-skateboard.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../../components/inputs/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-craft-skateboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './craft-skateboard.component.html',
   styleUrl: './craft-skateboard.component.css'
 })
 export class CraftSkateboardComponent {
-
+  selectedItems = [
+    {
+      name: 'board',
+      quantity: 1,
+      price: 200
+    },
+    {
+      name: 'trucks',
+      quantity: 2,
+      price: 500
+    },
+    {
+      name: 'wheels',
+      quantity: 4,
+      price: 500
+    },
+    {
+      name: 'bearings',
+      quantity: 4,
+      price: 500
+    },
+  ]
 }
