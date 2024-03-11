46fa22e93c9c6ca19e21db815629e648486a061b
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-11 19:31:06 +0200

    Selected Item Component Structure

--- client/src/app/pages/craft/windows/editor/editor.component.ts
+++ client/src/app/pages/craft/windows/editor/editor.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SelectedItemComponent } from '../selected-item/selected-item.component';
 
 @Component({
   selector: 'app-editor',
   standalone: true,
-  imports: [],
+  imports: [SelectedItemComponent],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.css'
 })
