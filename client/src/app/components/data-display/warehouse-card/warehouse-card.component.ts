85d70314c25e636b24ad843cac1176745c968bf4
Ungerer221 <221302@virtualwindow.co.za>
2024-03-12 19:57:27 +0200

    warehouse card dummy data setup

--- client/src/app/components/data-display/warehouse-card/warehouse-card.component.ts
+++ client/src/app/components/data-display/warehouse-card/warehouse-card.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { WarehouseData } from '../../../models/warehouse-data.model';
 @Component({
   selector: 'app-warehouse-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './warehouse-card.component.html',
   styleUrl: './warehouse-card.component.css'
 })
 export class WarehouseCardComponent {
 
+
+
+  // dummy data 
+  warehousecardData: WarehouseData[] = [
+    {
+      id: 1,
+      location: "pretoria",
+      boards: 3,
+      wheels: 4,
+      trucks: 6
+    },
+    {
+      id: 2,
+      location: "capetown",
+      boards: 3,
+      wheels: 4,
+      trucks: 6
+    },
+    {
+      id: 3,
+      location: "Johannesburg",
+      boards: 3,
+      wheels: 4,
+      trucks: 6
+    },
+  ]
 }
---
+++ client/src/app/models/warehouse-data.model.ts
@@ -0,0 +1,9 @@
+export interface WarehouseData {
+    // just testing the data not the final work
+    // also we need to boards and wheels and trucks 
+    id?: number;
+    location: string;
+    boards: number;
+    wheels:number;
+    trucks: number;
+}
