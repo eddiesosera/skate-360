c72eb8de848af9268c4f62837433b360a8fd5190
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-13 03:41:32 +0200

    Tool Option Images draft

---
+++ client/src/app/pages/craft/windows/tool-option/component/tool-option-card/tool-option-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-option-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './tool-option-card.component.html',
+  styleUrl: './tool-option-card.component.css'
+})
+export class ToolOptionCardComponent {
+
+}
--- client/src/app/pages/craft/windows/tool-option/tool-option.component.ts
+++ client/src/app/pages/craft/windows/tool-option/tool-option.component.ts
@@ -1,12 +1,88 @@
 import { Component } from '@angular/core';
+import { ToolOptionCardComponent } from './component/tool-option-card/tool-option-card.component';
 
 @Component({
   selector: 'app-tool-option',
   standalone: true,
-  imports: [],
+  imports: [ToolOptionCardComponent],
   templateUrl: './tool-option.component.html',
   styleUrl: './tool-option.component.css'
 })
 export class ToolOptionComponent {
+  componentType: string = 'Wheel';
+  selectedComponent = 'none'
+  inventoryList = [
+    {
+      type: 'wheels',
+      inventory: [
+        {
+          id: 0,
+          title: 'Default',
+          type: 'Standard',
+          image: '../../../../../assets/img/inventory/wheels/default.webp',
+          quantity: 20
+        },
+        {
+          id: 1,
+          title: 'Fuego',
+          type: 'Deluxe',
+          image: '../../../../../assets/img/inventory/wheels/fuego.png',
+          quantity: 20
+        },
+        {
+          id: 2,
+          title: 'Red Charger',
+          type: 'Deluxe',
+          image: '../../../../../assets/img/inventory/wheels/red_charger.png',
+          quantity: 20
+        }
+      ]
+    },
+    {
+      type: 'boards',
+      inventory: [
+        {
+          id: 0,
+          type: 'Classic',
+          image: '../../../../../assets/img/inventory/boards/classic_boards.jpeg',
+          quantity: 20
+        },
+        {
+          id: 1,
+          type: 'Old School',
+          image: '../../../../../assets/img/inventory/boards/oldschool_board.webp',
+          quantity: 20
+        },
+        {
+          id: 2,
+          type: 'Longboard',
+          image: '../../../../../assets/img/inventory/boards/longboard.jpg',
+          quantity: 20
+        }
+      ]
+    },
+    {
+      type: 'trucks',
+      inventory: [
+        {
+          id: 0,
+          type: 'Classic',
+          image: '../../../../../assets/img/inventory/truck.png',
+          quantity: 20
+        },
+      ]
+    },
+    {
+      type: 'bearings',
+      inventory: [
+        {
+          id: 0,
+          type: 'Classic',
+          image: '../../../../../assets/img/inventory/bearing.webp',
+          quantity: 20
+        },
+      ]
+    }
+  ]
 
 }
