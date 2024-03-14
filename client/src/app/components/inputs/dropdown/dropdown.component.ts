bcc8e96083be20e2b0ffdf5778feccbe9b13f966
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-14 08:01:13 +0200

    Dropdown Style
    Note: The frontend functionality good though needs a couple tweaks. Additionally the dropwdown will need to manage data

--- client/src/app/components/inputs/dropdown/dropdown.component.ts
+++ client/src/app/components/inputs/dropdown/dropdown.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dropdown.component.html',
-  styleUrl: './dropdown.component.css'
+  styleUrls: ['./dropdown.component.css']
 })
 export class DropdownComponent {
+  @Input() label = 'Option';
+  @Input() options = [
+    {
+      id: 0,
+      label: 'Option 1',
+      action: () => { alert(''); this.label = 'Option 1' }
+    },
+    {
+      id: 1,
+      label: 'Option 2',
+      action: () => { alert(''); this.label = 'Option 2' }
+    }
+  ]
+  isOpen = false;
 
+  toggleDropdown(): void {
+    this.isOpen = true;
+  }
+
+  closeDropdown(): void {
+    this.isOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    if (!event.target) return;
+    const target = event.target as HTMLElement;
+    if (!target.closest('.dropdown')) {
+      this.closeDropdown();
+    }
+  }
 }
--- client/src/app/pages/skateboards/skateboards.component.ts
+++ client/src/app/pages/skateboards/skateboards.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SidebarComponent } from '../../components/navigation/sidebar/sidebar.component';
 import { ButtonComponent } from '../../components/inputs/button/button.component';
+import { DropdownComponent } from '../../components/inputs/dropdown/dropdown.component';
 
 @Component({
   selector: 'app-skateboards',
   standalone: true,
-  imports: [RouterLink, SidebarComponent, ButtonComponent],
+  imports: [RouterLink, SidebarComponent, ButtonComponent, DropdownComponent],
   templateUrl: './skateboards.component.html',
   styleUrl: './skateboards.component.css'
 })
@@ -34,6 +35,51 @@ export class SkateboardsComponent {
         'eddie', 'ungerer', 'abraham'
       ]
     }
+  ];
+  @Input() label1: string = 'Type';
+  @Input() label2: string = 'Popularity';
+  @Input() label3: string = 'Created By';
+  filterOptions1 = [{
+    id: 0,
+    label: 'Classic',
+    action: () => { alert(''); this.label1 = 'Classic' }
+  },
+  {
+    id: 1,
+    label: 'Old School',
+    action: () => { alert(''); this.label1 = 'Old School' }
+  },
+  {
+    id: 2,
+    label: 'Long Board',
+    action: () => { alert(''); this.label1 = 'Long Board' }
+  }];
+
+  filterOptions2 = [
+    {
+      id: 0,
+      label: 'Least',
+      action: () => { alert(''); this.label1 = 'Classic' }
+    },
+    {
+      id: 1,
+      label: 'Most',
+      action: () => { alert(''); this.label1 = 'Old School' }
+    },
+  ];
+
+  filterOptions3 = [
+    {
+      id: 0,
+      label: 'Eddie',
+      action: () => { alert(''); this.label1 = 'Classic' }
+    },
+    {
+      id: 1,
+      label: 'Ungerer',
+      action: () => { alert(''); this.label1 = 'Old School' }
+    },
   ]
+
 
 }
