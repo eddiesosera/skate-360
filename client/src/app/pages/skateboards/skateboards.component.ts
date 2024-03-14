722eac9ffe82745636e9d67badcc215029e2f3c5
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-14 06:57:33 +0200

    Skateboards Heading Top Style + Dropdown init

--- client/src/app/pages/skateboards/skateboards.component.ts
+++ client/src/app/pages/skateboards/skateboards.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { SidebarComponent } from '../../components/navigation/sidebar/sidebar.component';
+import { ButtonComponent } from '../../components/inputs/button/button.component';
 
 @Component({
   selector: 'app-skateboards',
   standalone: true,
-  imports: [RouterLink, SidebarComponent],
+  imports: [RouterLink, SidebarComponent, ButtonComponent],
   templateUrl: './skateboards.component.html',
   styleUrl: './skateboards.component.css'
 })
 export class SkateboardsComponent {
+  filter = [
+    {
+      id: 'type',
+      name: 'Type',
+      options: [
+        'classic', 'long board', 'old school'
+      ]
+    },
+    {
+      id: 'popularity',
+      name: 'Popularity',
+      options: [
+        'least', 'most'
+      ]
+    },
+    {
+      id: 'craftedBy',
+      name: 'Crafted By',
+      options: [
+        // List of employees
+        'eddie', 'ungerer', 'abraham'
+      ]
+    }
+  ]
 
 }
