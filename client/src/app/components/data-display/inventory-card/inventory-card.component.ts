4a804b0d4200e1e882dd5ebcecddc2c0b166ed7e
Ungerer221 <221302@virtualwindow.co.za>
2024-03-18 12:37:58 +0200

    Generated Inventory Card

---
+++ client/src/app/components/data-display/inventory-card/inventory-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './inventory-card.component.html',
+  styleUrl: './inventory-card.component.css'
+})
+export class InventoryCardComponent {
+
+}
