40cfd2c6d69e2f1f3ff5ce3bffcc33d5d07dad15
Eddie Sosera <21100419@virtualwindow.co.za>
2024-03-22 01:14:07 +0200

    Onboarding Login Structure

--- client/src/app/pages/onboarding/onboarding.component.ts
+++ client/src/app/pages/onboarding/onboarding.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ButtonComponent } from '../../components/inputs/button/button.component';
 
 @Component({
   selector: 'app-onboarding',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ButtonComponent],
   templateUrl: './onboarding.component.html',
   styleUrl: './onboarding.component.css'
 })
 export class OnboardingComponent {
+  bgImg = '../../../assets/img/login_bg.png'
 
 }
