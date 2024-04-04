35e80e035c12fb6268a0d018b7e953c53b6578b0
Ungerer221 <221302@virtualwindow.co.za>
2024-04-04 22:06:32 +0200

    made new inventory card

--- client/src/app/components/data-display/inventory-card/inventory-card.component.ts
+++ client/src/app/components/data-display/inventory-card/inventory-card.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { Component, HostListener, Input } from '@angular/core';
+import { CardOptionsComponent } from '../product-card/card-options/card-options.component';
+
 
 @Component({
   selector: 'app-inventory-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardOptionsComponent],
   templateUrl: './inventory-card.component.html',
   styleUrl: './inventory-card.component.css'
 })
 export class InventoryCardComponent {
 
+
+  @Input() id: any
+  @Input() name: any
+  @Input() price: any
+  imageDeck = '../../../../assets/img/decks/classic.png'
+  cardWidth: any
 }
