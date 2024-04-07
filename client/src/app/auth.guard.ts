50a9f4af39ffed5da542c57688b7fe512ef24be7
Ungerer221 <221302@virtualwindow.co.za>
2024-04-07 16:39:37 +0200

    commented out the Auth For redirect

--- client/src/app/auth.guard.ts
+++ client/src/app/auth.guard.ts
@@ -1,5 +1,20 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const isLoggedIn = !!localStorage.getItem('loggedIn');
+    if (isLoggedIn) {
+      return true;
+    } else {
+      return this.router.createUrlTree(['/login']); // Redirect to login page if not logged in
+    }
+  }
+}
