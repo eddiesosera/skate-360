b55ec47f734ae9883d03b7af67d14f899766f245
Ungerer221 <221302@virtualwindow.co.za>
2024-04-07 17:28:07 +0200

    Fix for the Account page
    commented out all the auth Guard Code as  the redirect method isn't working and its causing the error.

--- client/src/app/auth.guard.ts
+++ client/src/app/auth.guard.ts
@@ -1,20 +1,20 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+// import { Injectable } from '@angular/core';
+// import { CanActivate, Router, UrlTree } from '@angular/router';
+// import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router) {}
+//   constructor(private router: Router) {}
 
-  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const isLoggedIn = !!localStorage.getItem('loggedIn');
-    if (isLoggedIn) {
-      return true;
-    } else {
-      return this.router.createUrlTree(['/login']); // Redirect to login page if not logged in
-    }
-  }
-}
+//   canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+//     const isLoggedIn = !!localStorage.getItem('loggedIn');
+//     if (isLoggedIn) {
+//       return true;
+//     } else {
+//       return this.router.createUrlTree(['/onboarding']); // Redirect to login page if not logged in
+//     }
+//   }
+// }
